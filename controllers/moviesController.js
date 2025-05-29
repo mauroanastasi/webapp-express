1551a8041fe1424b3c94eadeaf4e90f28e176fbd
mauroanastasi <mauroanastasi1@gmail.com>
2025-05-30 01:24:17 +0200

    [UPD] inserisco logica con query all'interno di show, test su postman ok

--- controllers/moviesController.js
+++ controllers/moviesController.js
@@ -2,17 +2,22 @@ const connection = require(`../data/db`)
 
 const index = (req, res) => {
     const sql = `SELECT * FROM movies`
-    connection.query(sql, (err, results) => {
-        if (err) {
-            return res.status(500).json({ error: `Database query failed` + err })
-        }
-        console.log(results)
-        res.json(results)
-    })
+    connection.query(sql, (err, moviesResult) => {
+        if (err) return res.status(500).json({ error: `Database query failed` + err })
+
+        console.log(moviesResult)
+        res.json(moviesResult)
+    });
 };
 
 const show = (req, res) => {
-    res.send(`Dettaglio del Film e recensione con id ${req.params.id}`)
+    const { id } = req.params
+    const moviesSql = `SELECT * FROM movies WHERE id = ?`
+
+    connection.query(moviesSql, [id], (err, moviesResult) => {
+        if (err) return res.status(500).json({ error: `Database query failed` + err })
+        res.json(moviesResult);
+    })
 };
 
 module.exports = { index, show }
