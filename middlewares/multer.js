19f5247f40beb7f5aa455ea6f4f2f871a033a6a6
mauroanastasi <mauroanastasi1@gmail.com>
2025-06-11 15:59:22 +0200

    [UPD] definisco l'oggetto storage del middleware che conterrà indirizzo del file che inseriremo, metodo con parametri per creare nome unico, e funzione cb che permette di andare avanti. Dichiaro poi variabile upload che contiene istanza di multer a cui passo storage

--- middlewares/multer.js
+++ middlewares/multer.js
@@ -1,1 +1,12 @@
 const multer = require(`multer`);
+
+const storage = multer.diskStorage({
+    destination: `/pubic/imgs/movies`,
+    filename: (req, file, cb) => {
+        const uniqueName = `${Date.now()}-${file.originalname.name}`
+        cb(null, uniqueName);
+    }
+})
+
+const upload = multer({ storage })
+module.exports = upload;
